8dde8090545a1f141326b4e0a20f52fcfd19a4fb
ujwalkaruna <karuna.ujwal@gmail.com>
2022-04-10 18:40:50 +0200

    var added

--- src/main/java/in/ashokit/Demo.java
+++ src/main/java/in/ashokit/Demo.java
@@ -6,6 +6,7 @@ public class Demo {
 	
 	public static void main(String[] args) {
             int i=10;
+int k=20;
 		String encodedTxt = PasswordService.encode("india@123");
 		System.out.println(encodedTxt);
 		String decodedTxt = PasswordService.decode(encodedTxt);
